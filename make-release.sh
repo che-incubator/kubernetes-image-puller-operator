bdb8e4de16be9190c5291e59f26408585bdc24fb
Anatolii Bazko <abazko@redhat.com>
2023-06-02 11:46:54 +0300

    fix: Release script
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -167,7 +167,7 @@ createPRToMainBranch() {
   resetChanges main
   local tmpBranch="copy-${RELEASE_VERSION}-bundle-to-main"
   git checkout -B "${tmpBranch}"
-  git diff refs/heads/main...refs/heads/${RELEASE_BRANCH} ':(exclude)bundle' | git apply -3
+  git diff refs/heads/main...refs/heads/${RELEASE_BRANCH} | git apply -3
   if git status --porcelain; then
     git add -A || true
     git commit -am "ci: Copy ${RELEASE_VERSION} bundle to main" --signoff
