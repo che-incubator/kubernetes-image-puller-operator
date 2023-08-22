fb5da49d2fc70751ec34177cf95f3eabe4b43cf8
Anatolii Bazko <abazko@redhat.com>
2023-08-22 16:15:45 +0200

    Fix tests
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- controllers/kubernetesimagepuller_controller_test.go
+++ controllers/kubernetesimagepuller_controller_test.go
@@ -613,7 +613,7 @@ func TestCreatesConfigMap(t *testing.T) {
 			want: &corev1.ConfigMap{
 				Data: map[string]string{
 					"CACHING_INTERVAL_HOURS": "1",
-					"CACHING_MEMORY_LIMIT":   "20Mi",
+					"CACHING_MEMORY_LIMIT":   "30Mi",
 					"CACHING_MEMORY_REQUEST": "10Mi",
 					"CACHING_CPU_LIMIT":      ".2",
 					"CACHING_CPU_REQUEST":    ".05",
@@ -661,7 +661,7 @@ func TestCreatesConfigMap(t *testing.T) {
 			want: &corev1.ConfigMap{
 				Data: map[string]string{
 					"CACHING_INTERVAL_HOURS": "1",
-					"CACHING_MEMORY_LIMIT":   "20Mi",
+					"CACHING_MEMORY_LIMIT":   "30Mi",
 					"CACHING_MEMORY_REQUEST": "10Mi",
 					"CACHING_CPU_LIMIT":      ".2",
 					"CACHING_CPU_REQUEST":    ".05",
@@ -707,7 +707,7 @@ func TestCreatesConfigMap(t *testing.T) {
 			want: &corev1.ConfigMap{
 				Data: map[string]string{
 					"CACHING_INTERVAL_HOURS": "1",
-					"CACHING_MEMORY_LIMIT":   "20Mi",
+					"CACHING_MEMORY_LIMIT":   "30Mi",
 					"CACHING_MEMORY_REQUEST": "10Mi",
 					"CACHING_CPU_LIMIT":      ".2",
 					"CACHING_CPU_REQUEST":    ".05",
@@ -801,7 +801,7 @@ func TestUpdatesConfigMap(t *testing.T) {
 				},
 				Data: map[string]string{
 					"CACHING_INTERVAL_HOURS": "1",
-					"CACHING_MEMORY_LIMIT":   "20Mi",
+					"CACHING_MEMORY_LIMIT":   "30Mi",
 					"CACHING_MEMORY_REQUEST": "10Mi",
 					"CACHING_CPU_LIMIT":      ".2",
 					"CACHING_CPU_REQUEST":    ".05",
@@ -845,7 +845,7 @@ func TestUpdatesConfigMap(t *testing.T) {
 				},
 				Data: map[string]string{
 					"CACHING_INTERVAL_HOURS": "1",
-					"CACHING_MEMORY_LIMIT":   "20Mi",
+					"CACHING_MEMORY_LIMIT":   "30Mi",
 					"CACHING_MEMORY_REQUEST": "10Mi",
 					"CACHING_CPU_LIMIT":      ".2",
 					"CACHING_CPU_REQUEST":    ".05",
