1e851e153a76e9c0eb21ba13f8f6ad926234039b
dkwon17 <dakwon@redhat.com>
2023-12-06 18:38:44 +0000

    Replace tabs with spaces for if else statements
    Signed-off-by: dkwon17 <dakwon@redhat.com>

--- build/make/deploy.mk
+++ build/make/deploy.mk
@@ -1,17 +1,17 @@
 ifeq (,$(shell which kubectl)$(shell which oc))
-  $(error oc or kubectl is required to proceed)
+    $(error oc or kubectl is required to proceed)
 endif
 
 ifneq (,$(shell which kubectl))
-	K8S_CLI := kubectl
+    K8S_CLI := kubectl
 else
-	K8S_CLI := oc
+    K8S_CLI := oc
 endif
 
 ifeq ($(shell $(K8S_CLI) api-resources --api-group='route.openshift.io' 2>&1 | grep -o routes),routes)
-	PLATFORM := openshift
+    PLATFORM := openshift
 else
-	PLATFORM := kubernetes
+    PLATFORM := kubernetes
 endif
 
 install: manifests download-kustomize ## Install CRDs into the K8s cluster specified in ~/.kube/config.
@@ -25,4 +25,3 @@ deploy: manifests download-kustomize kustomize-operator-image gen-deployment ## 
 
 undeploy: download-kustomize ## Undeploy controller from the K8s cluster specified in ~/.kube/config.
 	$(K8S_CLI) delete -f deploy/deployment/$(PLATFORM)/combined.yaml
-
